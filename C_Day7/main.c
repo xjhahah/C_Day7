c8eae3a2f27c58542a788f020dbdcc4e63118af5
xjhahah <908234867@qq.com>
2018-12-06 16:10:16 +0800

    提交代码

--- C_Day7/main.c
+++ C_Day7/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <math.h>
 
 //1 乘法口诀表
 void Test1()
@@ -26,9 +26,57 @@ void SwapTwoNum()
 	a = a ^ b;
 	printf("a=%d b=%d\n", a, b);
 }
+
+//3 实现一个函数判断year是不是润年。 
+void Test3()
+{
+	int year = 0;
+	printf("请输入你要查询的年份：");
+	scanf_s("%d", &year);
+	if (!(year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	{
+		printf("是闰年\n");
+	}
+	else {
+		printf("不湿润年\n");
+	}
+}
+
+//4 判断一个数是不是素数。
+int Prime()
+{
+	int i = 0;
+	printf("请输入你要判断数：");
+	scanf_s("%d", &i);
+	if (1 == i)
+		return 1;
+	for (int j = 2; j < sqrt(i); ++j)
+	{
+		if (i%j != 0)
+		{
+			return 1;
+		}
+	}
+	return -1;
+}
+void Test4()
+{
+	int ret = 0;
+	ret = Prime();
+	if (ret == 1)
+	{
+		printf("该数是素数...\n");
+	}
+	else
+	{
+		printf("该数不是素数\n");
+	}
+}
 int main()
 {
 	//Test1();
-	SwapTwoNum();
+	//SwapTwoNum();
+	//Test3();
+	Test4();
 	return 0;
 }
